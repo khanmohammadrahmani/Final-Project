9fa444556b7be4a9babb392a4a0aa9c36719e40b
khanmohammadrahmani <khanmohammadrahmani1@gmail.com>
2026-06-16 01:01:49 -0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+   <h1 className="text-4xl font-bold text-red-400">
+  Tailwind Working
+</h1>
+  )
+}
+export default App
