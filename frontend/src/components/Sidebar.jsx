b56575bc23f5f7a741b9e319a20c5b39663b46ec
khanmohammadrahmani <khanmohammadrahmani1@gmail.com>
2026-06-17 11:38:00 -0700

    Side Creted and Complted

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,1 +1,299 @@
-export default function Sidebar() {}
+import { useState, useEffect, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+import {
+  FiHome,
+  FiUsers,
+  FiPieChart,
+  FiChevronsDown,
+  FiChevronsUp,
+  FiSettings,
+  FiChevronLeft,
+  FiChevronRight,
+} from "react-icons/fi";
+import { Menu } from "lucide-react";
+
+import defaultAvatar from "../assets/images/user-def-image.webp";
+import { FaRegStickyNote } from "react-icons/fa";
+
+export default function Sidebar({ role }) {
+  const location = useLocation();
+  const sidebarRef = useRef();
+  const { t, i18n } = useTranslation();
+
+  const isRTL = i18n.language === "fa" || i18n.language === "ps";
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(null);
+
+  const [indicatorStyle, setIndicatorStyle] = useState({
+    top: 0,
+    height: 0,
+  });
+
+  const itemRefs = useRef({});
+
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+
+  // const BASE_URL = import.meta.env.VITE_API_URL;
+
+  const BASE_URL =
+    import.meta.env.VITE_IMAGE_URL || import.meta.env.VITE_API_URL;
+
+  // ================= RESPONSIVE =================
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) setSidebarOpen(false);
+      else setSidebarOpen(true);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // ================= ACTIVE INDICATOR =================
+  useEffect(() => {
+    const el = itemRefs.current[location.pathname];
+
+    if (el) {
+      setIndicatorStyle({
+        top: el.offsetTop,
+        height: el.offsetHeight,
+      });
+    }
+  }, [location.pathname, sidebarOpen, openMenu]);
+
+  // OUTSIDE CLICK - OK
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setSidebarOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const getAvatar = () => {
+    if (!user?.user_photo_url) return defaultAvatar;
+    if (user.user_photo_url.startsWith("http")) return user.user_photo_url;
+    return `${BASE_URL}${user.user_photo_url}`;
+  };
+
+  const isActive = (path) => location.pathname === path;
+
+  const toggleMenu = (name) => {
+    setOpenMenu((prev) => (prev === name ? null : name));
+  };
+
+  // ================= FUTURE SOCKET SAFE HOOK =================
+  useEffect(() => {
+    // reserved for socket integration later
+    // prevents future breaking changes
+  }, []);
+
+  const menuItems = {
+    // >>>>>>>>>>>>>>>> Sidebar for Admin Role:
+    Admin: [
+      {
+        name: "dashboard",
+        path: "/admin/dashboard",
+        icon: <FiPieChart className="animate-bounce" />,
+      },
+      {
+        name: "company",
+        icon: <FiHome className="animate-bounce" />,
+        submenu: [
+          {
+            name: "company_info",
+            path: "/admin/company/company-info",
+          },
+          {
+            name: "company_documents",
+            path: "/admin/company/company-documents",
+          },
+        ],
+      },
+
+      // ----------------------------------------------------------------------------------------------
+      {
+        name: "system",
+        icon: <FiSettings className="animate-spin" />,
+        submenu: [
+          { name: "user_accounts", path: "/admin/users/users-list" },          
+        ],
+      },
+    ],
+
+    // >>>>>>>>>>>>>>>> Sidebar for HR Role:
+    HR: [
+      { name: "Dashboard", path: "/hr/dashboard", icon: <FiPieChart /> },
+      {
+        name: "hr",
+        icon: <FiUsers className=" animate-bounce" />,
+        submenu: [
+          { name: "employees", path: "/hr/employees/employee-list" },
+          {
+            name: "employee_education",
+            path: "/hr/employees/employee-education-info",
+          },
+          {
+            name: "salary_payment",
+            path: "/hr/employees/employee-salary-payment",
+          },
+        ],
+      },
+    ],
+  };
+
+  const isAnyChildActive = (item) => {
+    if (!item.submenu) return false;
+    return item.submenu.some((sub) => sub.path === location.pathname);
+  };
+  return (
+    <>
+      {/* <div  style={{marginRight: sidebarOpen ? "12rem" : "",    transition: "ease 0.5s",    }}      > */}
+      {/* TOGGLE */}
+      <div
+        onClick={() => setSidebarOpen(!sidebarOpen)}
+        className="fixed top-3 z-[9999] w-7 h-7 flex items-center justify-center bg-white border text-black shadow-sm rounded-full cursor-pointer hover:bg-blue-100"
+        style={{
+          left: !isRTL ? (sidebarOpen ? "13.5rem" : "0.3rem") : "auto",
+          right: isRTL ? (sidebarOpen ? "13.5rem" : "0.3rem") : "auto",
+        }}
+      >
+        {sidebarOpen ? (
+          isRTL ? (
+            <FiChevronRight size={18} />
+          ) : (
+            <FiChevronLeft size={18} />
+          )
+        ) : isRTL ? (
+          <Menu size={19} />
+        ) : (
+          <Menu size={19} />
+        )}
+      </div>
+
+      {/* BACKDROP */}
+      {sidebarOpen && (
+        <div
+          onClick={() => setSidebarOpen(false)}
+          className="fixed inset-0 bg-black/10 md:hidden z-40"
+        />
+      )}
+
+      {/* SIDEBAR */}
+      <div
+        ref={sidebarRef}
+        className="fixed top-0 h-screen bg-white text-black shadow-lg z-50 overflow-hidden transition-all duration-300"
+        style={{
+          width: sidebarOpen ? "14.5rem" : "0rem",
+          left: isRTL ? "auto" : 0,
+          right: isRTL ? 0 : "auto",
+        }}
+      >
+        {/* HEADER */}
+        <div className="p-3 border-b flex items-center gap-3 bg-zinc-100 rounded">
+          <img
+            src={getAvatar()}
+            onError={(e) => (e.target.src = defaultAvatar)}
+            className="w-12 h-12 rounded-full object-cover border shadow"
+          />
+
+          <div className="flex flex-col min-w-0">
+            <h1 className="text-sm font-bold text-blue-600">CC-MIS</h1>
+            <p className="text-sm font-semibold truncate">{user.user_name}</p>
+            <p className="text-[11px] truncate text-gray-500">
+              {user.user_email}
+            </p>
+          </div>
+        </div>
+
+        {/* MENU */}
+        <div className="relative p-2 text-sm space-y-1 overflow-y-auto h-[calc(100%-80px)] scrollbar-hide">
+          <div
+            className="absolute w-1 bg-blue-500 rounded-full transition-all duration-300"
+            style={{
+              top: indicatorStyle.top,
+              height: indicatorStyle.height,
+              left: isRTL ? "auto" : 0,
+              right: isRTL ? 0 : "auto",
+            }}
+          />
+
+          {menuItems[role]?.map((item, i) => (
+            <div key={i}>
+              {item.path ? (
+                <Link
+                  ref={(el) => (itemRefs.current[item.path] = el)}
+                  to={item.path}
+                  className={`flex items-center gap-3 px-3 py-2 rounded-lg ${
+                    isActive(item.path)
+                      ? "bg-blue-50 text-blue-600 font-semibold"
+                      : "hover:bg-gray-100"
+                  }`}
+                >
+                  {item.icon}
+                  <span>{t(item.name)}</span>
+                </Link>
+              ) : (
+                <>
+                  <div
+                    ref={(el) => {
+                      if (isAnyChildActive(item)) {
+                        itemRefs.current[item.name] = el;
+                      }
+                    }}
+                    onClick={() => toggleMenu(item.name)}
+                    className={`flex items-center gap-3 px-3 py-2 rounded-lg cursor-pointer ${
+                      isAnyChildActive(item)
+                        ? "bg-blue-50 text-blue-600 font-semibold"
+                        : "hover:bg-gray-100"
+                    }`}
+                  >
+                    {item.icon}
+                    <span className="flex-1">{t(item.name)}</span>
+                    {openMenu === item.name ? (
+                      <FiChevronsUp />
+                    ) : (
+                      <FiChevronsDown />
+                    )}
+                  </div>
+
+                  {/* Menu Items / Sub menu */}
+                  {openMenu === item.name && (
+                    <div
+                      className={`mt-1 space-y-1 ${isRTL ? "mr-8" : "ml-8"}`}
+                    >
+                      {item.submenu.map((sub, j) => (
+                        <Link
+                          key={j}
+                          ref={(el) => (itemRefs.current[sub.path] = el)}
+                          to={sub.path}
+                          className={`block px-2 py-1 rounded ${
+                            isActive(sub.path)
+                              ? "bg-gray-100 text-blue-600 font-semibold "
+                              : "hover:bg-gray-100"
+                          }`}
+                        >
+                          {t(sub.name)}
+                        </Link>
+                      ))}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* </div> */}
+    </>
+  );
+}
